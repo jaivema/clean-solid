6f132fb909f707f9bfefcf23591aff1623a04e35
jverdejo <jverdejo@eqtic.net>
2025-02-25 16:45:45 +0100

    end product class no dry example

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -1,22 +1,40 @@
 
-type Size = ''|'XS'|'S'|'M'|'L'|'XL'|'XXL';
+type Size = '' | 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL';
 
 class Product {
-    constructor( 
+    constructor(
         public name: string = '',
         public price: number = 0,
         public size: Size = '',
-    ){}
+    ) { }
 
-    toString(){
-        if ( this.name.length <= 0) throw Error('Name is empty.');
-        if ( this.price <= 0)       throw Error('Price is empty.');
-        if ( this.size.length <= 0) throw Error('Size is empty.');
-        return `${ this.name } (${ this.size }) - €${ this.price }`;
+    private isProductReady(): boolean {
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if (this[key].length <= 0) throw Error(`${key} is empty.`);
+                    break;
+                case 'number':
+                    if (this[key] <= 0) throw Error(`${key} is zero.`);
+                    break;
+                default:
+                    throw Error(`${typeof this[key]} is not allowed.`);
+            }
+        }
+        return true;
+    }
+    toString() {
+        // No Dry
+        // if ( this.name.length <= 0) throw Error('Name is empty.');
+        // if ( this.price <= 0)       throw Error('Price is empty.');
+        // if ( this.size.length <= 0) throw Error('Size is empty.');
+
+        if (!this.isProductReady()) return 'Product is not ready';
+        return `${this.name} (${this.size}) - €${this.price}`;
     }
 }
 
-(()=>{
-    const redTShirt = new Product('Red T-Shirt', 10, '');
+(() => {
+    const redTShirt = new Product('Red T-Shirt', 10, 'M');
     console.log(redTShirt.toString());
 })();
